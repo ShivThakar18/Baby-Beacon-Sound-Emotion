c6a15e884f5fdda106d1aa848a2a6302c5097297
thakas4 <thakas4@mcmaster.ca>
2025-02-05 16:13:59 -0500

    convert h5 to pb file for openCV

--- python/h5_to_onnx.py
+++ python/h5_to_onnx.py
@@ -1,17 +1,10 @@
 import tensorflow as tf
-import tf2onnx
+import keras
+h5_model = "C:\\Users\\shivt\\Documents\\GitHub\\Baby-Beacon-Sound-Emotion\\python\\model.h5"
 
-# Load the Keras model
-model = tf.keras.models.load_model("C:\\Users\\shivt\\Documents\\GitHub\\Baby-Beacon-Sound-Emotion\\model\\augmented_baby_emotions_model.h5")  # or "your_model.keras"
+model = keras.models.load_model(h5_model)
 
-# Convert to ONNX
-onnx_model_path = "C:\\Users\\shivt\\Documents\\GitHub\\Baby-Beacon-Sound-Emotion\\model\\augmented_baby_emotions_model.onnx"  # Ensure the extension is .onnx
+pb = "model_saved"
 
-# Convert model
-spec = (tf.TensorSpec((None, 40), tf.float32),)  # Adjust input shape to match your MFCC features
-onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=spec, opset=13)
-
-# Save the ONNX model
-tf2onnx.save_model(onnx_model, onnx_model_path)
-
-print(f"Model saved to {onnx_model_path}")
+tf.saved_model.save(model,pb)
+print("saved")
