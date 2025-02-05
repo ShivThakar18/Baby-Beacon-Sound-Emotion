2b0786ce18274ac99eb0000bcee32b3b42c20f89
thakas4 <thakas4@mcmaster.ca>
2025-02-05 17:01:39 -0500

    convert pb SavedModel to pb Frozen Model

--- python/h5_to_onnx.py
+++ python/h5_to_onnx.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
-import keras
-h5_model = "C:\\Users\\shivt\\Documents\\GitHub\\Baby-Beacon-Sound-Emotion\\python\\model.h5"
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-model = keras.models.load_model(h5_model)
+PATH = "C:\\Users\\shivt\\Documents\\GitHub\\Baby-Beacon-Sound-Emotion\\model\\"
+pb_model = PATH+"model_saved\\"
 
-pb = "model_saved"
+loaded_model = tf.saved_model.load(pb_model)
+print("pb Model loaded successfully")
 
-tf.saved_model.save(model,pb)
-print("saved")
+concerte_func = loaded_model.signatures['serving_default']
+frozen_func = convert_variables_to_constants_v2(concerte_func)
+
+tf.io.write_graph(frozen_func.graph,".",PATH+"frozen_model.pb", as_text=False)
+print("Frozen model saved as ../model/frozen_model.pb")
